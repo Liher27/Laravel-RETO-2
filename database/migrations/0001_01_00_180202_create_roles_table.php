ff71a678ece624587501df82d263336fff36aba7
Cartiled <yifeiye2002@gmail.com>
2024-12-02 19:08:59 +0100

    cambios de migrations

--- database/migrations/0001_01_00_180202_create_roles_table.php
+++ database/migrations/0001_01_00_180202_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-           
+            $table->String('role_name');
         });
     }
 
