2c91f62be0518bd01e4aacb5db2346885ec8d8ce
Cartiled <yifeiye2002@gmail.com>
2024-12-04 15:57:24 +0100

    saved

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Course extends Model
 {
+
+  public function subjects(): HasMany {
+    return $this->hasMany(Subject::class);
+}
   use SoftDeletes;
 }
--- app/Models/user_subject.php
+++ app/Models/user_subject.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class user_subject extends Model
 {
-    //
+    public function subjects(): HasMany {
+        return $this->hasMany(Subject::class);
+    }
+    public function users(): HasMany {
+        return $this->hasMany(User::class);
+    }
 }
