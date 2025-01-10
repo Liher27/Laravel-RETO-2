b7cb2ff046cbd99760d4958b06d93f41d4785ff4
Liher27 <liher.chamorropa@elorrieta-errekamari.com>
2025-01-10 19:26:58 +0100

    for merch

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<p>i m a admin</p>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,27 +18,32 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+    <div id="app" >
+        <nav class="navbar navbar-light" style="background-color:rgb(111, 184, 202);">
+        <a href="{{ url('/') }}">
+                <img class="img-fluid" src="{{ asset('EEM-logo-color.svg') }}" alt="EEM Logo" style="width: 175px; height: auto;">
+            </a>    
+        <div class="container">
+            
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                    <a>
+                <select class="form-select" aria-label="Small select example">
+                    <option selected>Castellano</option>
+                    <option value="1">Euskera</option>
+                </select>
+                </a>
                         <!-- Authentication Links -->
                         @guest
-                               
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -61,11 +66,14 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('courses.index') }}">{{ __('Modelos') }}</a>
                                 </li>
+                                
                             
                             <li class="nav-item dropdown">
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
+                    
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                
                                     <a class="dropdown-item" href="{{ route('logout') }}"
@@ -73,7 +81,6 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -81,6 +88,7 @@
                                         @csrf
                                     </form>
                                 </div>
+                                
                             </li>
                         @endguest
                     </ul>
